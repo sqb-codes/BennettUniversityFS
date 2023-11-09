26c7a9d58a317b69d9a1097b9da4af048d23b3a0
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2023-11-09 15:21:36 +0530

    Add files via upload

--- Group-G2/Javascript/BasicCalculator/calc.js
+++ Group-G2/Javascript/BasicCalculator/calc.js
@@ -5,16 +5,41 @@ window.addEventListener("load", initEvents);
 // we are adding an event "load" on windows
 // it means window will allow browser to load HTML first then it calls initEvent function
 
+var fnum;
+var snum;
+
+// bind all events in this function
 function initEvents() {
-    // alert("JS Loaded...");
+    fnum = document.querySelector("#box-1");
+    snum = document.querySelector("#box-2");
+
     document.querySelector("#add").addEventListener("click", add);
+    document.querySelector("#sub").addEventListener("click", sub);
+    document.querySelector("#div").addEventListener("click", div);
+    document.querySelector("#mul").addEventListener("click", mul);
 }
 
 
 function add() {
-    var fnum = document.querySelector("#box-1").value;
-    var snum = document.querySelector("#box-2").value;
     // parseInt - type cast into integer
-    var result = parseInt(fnum) + parseInt(snum);
+    var result = parseInt(fnum.value) + parseInt(snum.value);
+    document.querySelector("#box-3").value = result;
+}
+
+function sub() {
+    // parseInt - type cast into integer
+    var result = parseInt(fnum.value) - parseInt(snum.value);
+    document.querySelector("#box-3").value = result;
+}
+
+function div() {
+    // parseInt - type cast into integer
+    var result = parseInt(fnum.value) / parseInt(snum.value);
+    document.querySelector("#box-3").value = result;
+}
+
+function mul() {
+    // parseInt - type cast into integer
+    var result = parseInt(fnum.value) * parseInt(snum.value);
     document.querySelector("#box-3").value = result;
 }
