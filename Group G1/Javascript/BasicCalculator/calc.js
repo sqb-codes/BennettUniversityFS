ada1edf8ce4f4aca498cc5b08a417fdcc253d677
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2023-11-10 15:17:48 +0530

    Add files via upload

--- Group G1/Javascript/BasicCalculator/calc.js
+++ Group G1/Javascript/BasicCalculator/calc.js
@@ -5,14 +5,34 @@
 // so first HTML will be loaded and then JS
 window.addEventListener("load", initEvents);
 
+var fnum;
+var snum;
+
 function initEvents() {
+    fnum = document.querySelector("#box-1");
+    snum = document.querySelector("#box-2");
     document.querySelector("#add").addEventListener("click", add);
+    document.querySelector("#sub").addEventListener("click", sub);
+    document.querySelector("#div").addEventListener("click", div);
+    document.querySelector("#mul").addEventListener("click", mul);
 }
 
 function add() {
-    // console.log("Clicked on add button...");
-    var fnum = document.querySelector("#box-1");
-    var snum = document.querySelector("#box-2");
     var result = parseInt(fnum.value) + parseInt(snum.value);
+    document.querySelector("#box-3").value = result;
+}
+
+function sub() {
+    var result = parseInt(fnum.value) - parseInt(snum.value);
+    document.querySelector("#box-3").value = result;
+}
+
+function div() {
+    var result = parseInt(fnum.value) / parseInt(snum.value);
+    document.querySelector("#box-3").value = result;
+}
+
+function mul() {
+    var result = parseInt(fnum.value) * parseInt(snum.value);
     document.querySelector("#box-3").value = result;
 }
