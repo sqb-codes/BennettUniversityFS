7cd3c785116b993d512c370dc12e18fc678bd047
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2023-11-22 15:02:27 +0530

    Add files via upload

--- Group-G3/Javascript/AdvCalculator/calc.js
+++ Group-G3/Javascript/AdvCalculator/calc.js
@@ -1,21 +1,35 @@
 window.addEventListener("load", initEvents);
 
+var expression = "";
+var box;
 function initEvents() {
+    box = document.querySelector("#box");
+    document.querySelector(".equals").addEventListener("click", evaluate);
+    let numBtns = document.querySelectorAll(".num");
+    let oprBtns = document.querySelectorAll(".opr");
+    for (const btn of numBtns) {
+        btn.addEventListener("click", appendNumber);
+    }
 
+    for (const btn of oprBtns) {
+        btn.addEventListener("click", appendOperator);
+    }
 }
 
 function appendNumber() {
-
+    box.value += this.innerHTML;
+    expression = box.value;
 }
 
 function appendOperator() {
-
+    box.value = expression + this.innerHTML;
 }
 
 function evaluate() {
-
+    box.value = eval(expression);
+    expression = "";
 }
 
 function clear() {
-    
+
 }
